46e4146712373c6e7688b5d4fe3047931ec6ed6b
HyeranShin <hyebaragi__@naver.com>
2018-07-17 15:36:23 +0900

    새 프로젝트 생성, 한글 처리

---
+++ src/TestProject/Test.java
@@ -0,0 +1,13 @@
+package TestProject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// 1. 깨지는 것 대비 > 텍스트엔코딩 설정 > utf-8
+		// Window > Preferences > General > Workspace
+		// 2. 글자폰트 변경
+		// Window > Preferences > General > Colors and Fonts > Basic > Text Font
+		System.out.println("안녕 안드로이드!");
+	}
+
+}
