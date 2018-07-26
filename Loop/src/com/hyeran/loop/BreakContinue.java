bd343a33afce97382de94106a59bb741dea55601
HyeranShin <hyebaragi__@naver.com>
2018-07-26 18:39:22 +0900

    break_continue

---
+++ Loop/src/com/hyeran/loop/BreakContinue.java
@@ -0,0 +1,39 @@
+package com.hyeran.loop;
+/**
+ * break와 continue
+ * 
+ * break -> for문 종료
+ * 	continue ->  해당 조건만 생략	
+ * 
+ * @author hyeran
+ *
+ */
+public class BreakContinue {
+
+	public static void main(String[] args) {
+
+		int begin = 1;	// 시작하는 단
+		int end = 9;		// 마지막 단
+		int offset = 10;	// 한단에 출력하는 개수
+		
+		// 구구단 코드
+		// 레이블
+		out : for (int a = begin; a <= end; a = a+1) {
+			
+			// if (a > 5) break;
+			
+			for (int b = 1; b <= offset; b = b+1) {
+				
+				// if (b > 7) break; // 7 이후 출력 X
+				// if (b == 7) continue;	// 7만 생략
+				
+				if (b == 7) break out;	// 외부 for문 종료
+				
+				int c = a * b;
+				System.out.printf("%d X %d = %d \n", a, b, c);
+			}
+		}
+
+	}
+
+}
