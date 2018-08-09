4e83079fe85f56779f9c0296365d1a47cd8b549a
HyeranShin <hyebaragi__@naver.com>
2018-08-09 23:49:49 +0900

    객체지향 설계도구

--- Design Tools/src/com/hyeran/tools/ToolMain.java
+++ Design Tools/src/com/hyeran/tools/ToolMain.java
@@ -13,15 +13,57 @@ public class ToolMain {
 		// 1. interface
 		// 설계자의 의도대로 기능을 구현하도록 강제하는 것
 		// 설계자가 미리 정해놓은 함수 이름
-		Child child = new Child();
-		child.function();
-		child.open();
+//		Child child = new Child();
+//		child.function();
+//		child.open();
 		
-		// 2. abstract
+		// 2. abstract (추상 클래스, 추상 함수)
+		// 설계자의 의도대로 기능을 구현하도록 강제하는 것
+		// 설계자가 미리 정해놓은 함수 이름 + 로직(연산이 일어난 코드)
+		// 초기화 할 수 없다. 상속을 통해서만 사용 가능
+		
+		// Abstract abst = new Abstract(); X
+		
+		Abstract child = new AbstractChild();
+		// 타입이 Abstract인 경우 AbstractChild에서 새로 만든 함수는 사용 불가
+		child.open();
+		child.close();
 		
 		// 3. annotation
 	}
 }
+
+// 1. abstract 만들기
+
+abstract class Abstract {
+	
+	abstract public void open();
+	
+	// 일반 함수도 만들 수 있음
+	public void close() {
+		System.out.println("문이 닫혔습니다.");
+	}
+}
+
+// 2. abstract 사용하기 > 상속과 동일
+class AbstractChild extends Abstract {
+
+	@Override
+	public void open() {
+		System.out.println("문이 열렸습니다.");
+	}
+	
+	public void something() {
+		
+	}
+}
+
+
+
+
+
+
+
 
 // 1. 인터페이스 만들기
 // 인터페이스 이름은 주로 Interface의 약자인 I를 붙여서 설계
