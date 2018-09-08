f4a3f5e5384f4b4fd636de31d986d9ce3e02a648
HyeranShin <hyebaragi__@naver.com>
2018-09-08 13:14:41 +0900

    이미지리소스 사용하기

--- ANDROID/Widget/app/src/main/java/com/hyeran/android/widget/ImageActivity.java
+++ ANDROID/Widget/app/src/main/java/com/hyeran/android/widget/ImageActivity.java
@@ -9,5 +9,15 @@ public class ImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        // 이미지 리소스 디렉토리 구성
+        // 해상도가 높고 낮음을 나타냄
+        // ldpi - low dpi
+        // mdpi - mid dpi - 기준 해상도 (320 X 480) - 실제 이미지의 1px이 화면의 1px과 같다.
+        // hdpi - high dpi - 실제 이미지의 1.5px이 화면의 1px과 같다.
+        // 고해상도
+        // xhdpi - 실제 이미지의 2px이 화면의 1px과 같다.
+        // xxhdpi - 갤럭시 7,8 - 실제 이미지의 3px이 화면의 1px과 같다.
+        // xxxhdpi - 노트 - 실제 이미지의 4px이 화면의 1px과 같다.
     }
 }
