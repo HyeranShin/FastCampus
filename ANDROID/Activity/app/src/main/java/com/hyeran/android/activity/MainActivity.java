bba4ccffedcb4b2239e1b768ba83b73c69716bae
HyeranShin <hyebaragi__@naver.com>
2018-09-13 11:09:49 +0900

    액티비티 사이에 값 주고 받기

--- ANDROID/Activity/app/src/main/java/com/hyeran/android/activity/MainActivity.java
+++ ANDROID/Activity/app/src/main/java/com/hyeran/android/activity/MainActivity.java
@@ -5,15 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText toSecond, fromSecond;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toSecond = findViewById(R.id.toSecond);
+        fromSecond = findViewById(R.id.fromSecond);
         button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -22,9 +27,28 @@ public class MainActivity extends AppCompatActivity {
                 // 다른 액티비티 호출하기 - 시스템에 다른 액티비티를 호출하라고 메시지를 전달
                 // 1. Intent 생성
                 Intent intent = new Intent(getBaseContext(), SecondActivity.class);
-                // 2. 안드로이드에 액티비티를 실행해달라고 intent를 전달
-                startActivity(intent);
+
+                // 1.1 Intent에 값 담아서 전달하기
+                String value = toSecond.getText().toString();
+                intent.putExtra("key01", value);
+
+                // 2. 안드로이드에 액티비티를 실행해달라고 Intent를 전달
+                startActivityForResult(intent, 999);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // super.onActivityResult(requestCode, resultCode, data);
+        // 1. request 코드 체크: 실제 내가 호출한 결과 처리인지 확인
+        if(requestCode == 999) {
+            // 2. 정상적으로 결과 값이 넘어왔는지 검사
+            if(resultCode == RESULT_OK) {
+                // 3. 넘어온 결과값 Intent에서 값을 꺼내서 처리
+                String temp = data.getStringExtra("key02");
+                fromSecond.setText(temp);
+            }
+        }
     }
 }
--- ANDROID/Activity/app/src/main/java/com/hyeran/android/activity/SecondActivity.java
+++ ANDROID/Activity/app/src/main/java/com/hyeran/android/activity/SecondActivity.java
@@ -1,13 +1,58 @@
 package com.hyeran.android.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView valueText;
+    Button btnFinish;
+    EditText toMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        valueText = findViewById(R.id.valueText);
+        btnFinish = findViewById(R.id.btnFinish);
+        toMain = findViewById(R.id.toMain);
+
+        // 1. MainActivity에서 전달된 Intent 꺼내기
+        Intent intent = getIntent();
+
+        // 2. Intent에 담긴 값 중에 key01을 가진 값 꺼내기
+        String value = intent.getStringExtra("key01");
+
+        // 3. 꺼낸 값 화면에 표시하기
+        valueText.setText(value);
+
+        // 4. SecondActivity 종료하기
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SecondActivity의 editText에 있는 값을 나를 호출한 액티비티로 돌려주기
+                try {
+                    // 4.1 값 가져오기
+                    String temp = toMain.getText().toString();
+                    // 4.2 인텐트를 생성하고
+                    Intent intent = new Intent();   // 메세지 객체 생성
+                    // 4.3 인텐트에 값을 담고
+                    intent.putExtra("key02", temp);
+                    // 4.4 결과 메세지를 전달
+                    setResult(RESULT_OK, intent);
+                } catch(Exception e) {
+                    setResult(RESULT_CANCELED);
+                }
+
+                finish();
+                //SecondActivity.this.finish();
+            }
+        });
     }
 }
