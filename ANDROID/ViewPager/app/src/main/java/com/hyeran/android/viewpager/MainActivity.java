c172ffb23d4dfcf80d7234b85b02af6619efa243
HyeranShin <hyebaragi__@naver.com>
2018-10-02 21:41:18 +0900

    DONE < ViewPager + TabLayout >

--- ANDROID/ViewPager/app/src/main/java/com/hyeran/android/viewpager/MainActivity.java
+++ ANDROID/ViewPager/app/src/main/java/com/hyeran/android/viewpager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hyeran.android.viewpager;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -13,6 +14,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
+    TabLayout tabLayout;
     List<Fragment> fragments = new ArrayList<>();
 
     @Override
@@ -26,12 +28,21 @@ public class MainActivity extends AppCompatActivity {
         fragments.add(new TwoFragment());
         fragments.add(new ThreeFragment());
         fragments.add(new FourFragment());
-
+        // 화면 연결
         viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("One"));
+        tabLayout.addTab(tabLayout.newTab().setText("Two"));
+        tabLayout.addTab(tabLayout.newTab().setText("Three"));
+        tabLayout.addTab(tabLayout.newTab().setText("Four"));
         // 어댑터를 생성
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), fragments);
         // viewPager에 어댑터를 연결
         viewPager.setAdapter(adapter);
+        // 페이저가 변경되었을 때 탭을 자동으로 변경해주는 리스너
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        // 탭을 변경했을 때 뷰페이저에 반영해주는 리스너
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
     }
 }
 
